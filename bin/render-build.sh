0593657d6435f78bb5e450defbdb2a2465e3e3d3
kawamuratakuya <kawamuratakuya@kawamuratakuyanoMacBook-Air.local>
2026-02-20 23:27:02 +0900

    Fix Render build script (use bundle exec rails)

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,11 +1,9 @@
-
 #!/usr/bin/env bash
 set -o errexit
 
 bundle install
 
-bin/rails assets:precompile
-bin/rails assets:clean
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
 
-bin/rails db:migrate
-
+bundle exec rails db:migrate
