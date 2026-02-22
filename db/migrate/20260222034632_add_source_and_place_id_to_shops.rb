157ebaf86740431a55aa2330bf4d41c205b98877
kawamuratakuya <kawamuratakuya@kawamuratakuyanoMacBook-Air.local>
2026-02-22 22:14:29 +0900

    Fix duplicate column migration

--- db/migrate/20260222034632_add_source_and_place_id_to_shops.rb
+++ db/migrate/20260222034632_add_source_and_place_id_to_shops.rb
@@ -1,6 +1,11 @@
-class AddSourceAndPlaceIdToShops < ActiveRecord::Migration[8.1]
-  def change
-    add_column :shops, :source, :string
-    add_column :shops, :place_id, :string
-  end
+class AddSourceAndPlaceIdToShops < ActiveRecord::Migration[7.1]
+def change
+unless column_exists?(:shops, :source)
+add_column :shops, :source, :string
+end
+
+unless column_exists?(:shops, :place_id)
+add_column :shops, :place_id, :string
+end
+end
 end
