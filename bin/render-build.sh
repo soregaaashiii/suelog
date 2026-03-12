54f5f14b48dbb2ffed0889d39fe4b4700bb9d61a
soregaaashiii <abclologun@gmail.com>
2026-03-13 06:54:46 +0900

    Fix assets precompile on Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,6 @@ set -o errexit
 
 bundle install
 
-bundle exec rails assets:precompile
+bundle exec rails assets:precompile RAILS_ENV=production SECRET_KEY_BASE=dummy
+
 bundle exec rails assets:clean
