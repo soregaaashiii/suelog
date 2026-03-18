f0e70aa4a8a47c17cc0c76072775f0398be0fc8f
soregaaashiii <abclologun@gmail.com>
2026-03-19 03:54:06 +0900

    Fix Render deploy config

--- config/puma.rb
+++ config/puma.rb
@@ -9,16 +9,4 @@ environment ENV.fetch("RAILS_ENV", "development")
 
 pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 
-# developmentは必ず single mode
-if ENV.fetch("RAILS_ENV", "development") == "development"
-workers 0
-else
-workers ENV.fetch("WEB_CONCURRENCY", 1).to_i
-preload_app!
-end
-
 plugin :tmp_restart
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
-
-
-
