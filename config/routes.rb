e36fa45b7649ae25c800763e6509677138a7b5e7
soregaaashiii <abclologun@gmail.com>
2026-03-29 21:22:44 +0900

    Add Umeda and Namba area routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-# /Users/kawamuratakuya/dev/suelog/config/routes.rb
+# /Users/kawamuratakuya/Desktop/吸えログデータ/dev/suelog/config/routes.rb
 Rails.application.routes.draw do
 # onrender.com で来たアクセスを独自ドメインへ恒久リダイレクト
 constraints(host: /(?:.+\.)?onrender\.com\z/) do
@@ -10,6 +10,37 @@ end
 root "home#index"
 
 get "map", to: "maps#index"
+
+# ===== Area landing / filtered pages =====
+get "umeda",
+to: "home#index",
+as: :umeda,
+defaults: { area: "umeda" }
+
+get "umeda/smoking/:smoking_area",
+to: "home#index",
+as: :umeda_smoking,
+defaults: { area: "umeda" }
+
+get "umeda/genre/:genre",
+to: "home#index",
+as: :umeda_genre,
+defaults: { area: "umeda" }
+
+get "namba",
+to: "home#index",
+as: :namba,
+defaults: { area: "namba" }
+
+get "namba/smoking/:smoking_area",
+to: "home#index",
+as: :namba_smoking,
+defaults: { area: "namba" }
+
+get "namba/genre/:genre",
+to: "home#index",
+as: :namba_genre,
+defaults: { area: "namba" }
 
 resources :shops do
 resources :reviews, only: [:create]
