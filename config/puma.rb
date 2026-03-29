56212eb1d523541efd9ae0de51dea17e358356bb
soregaaashiii <abclologun@gmail.com>
2026-03-30 08:08:05 +0900

    Run Puma in single mode

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,17 @@
+# /Users/kawamuratakuya/Desktop/吸えログデータ/dev/suelog/config/puma.rb
 # frozen_string_literal: true
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
-threads threads_count, threads_count
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count).to_i
+threads min_threads_count, max_threads_count
 
 port ENV.fetch("PORT", 3000)
 
 environment ENV.fetch("RAILS_ENV", "development")
 
 pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
+
+# Starter 環境での不安定化切り分けのため single mode に固定
+workers 0
 
 plugin :tmp_restart
