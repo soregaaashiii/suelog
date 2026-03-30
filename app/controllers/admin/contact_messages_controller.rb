9bf8fb8dd5bc4854c7b8d582074653b9bc7d569c
soregaaashiii <abclologun@gmail.com>
2026-03-31 06:16:39 +0900

    Fix admin contact messages page

--- app/controllers/admin/contact_messages_controller.rb
+++ app/controllers/admin/contact_messages_controller.rb
@@ -1,15 +1,16 @@
+# /Users/kawamuratakuya/Desktop/吸えログデータ/dev/suelog/app/controllers/admin/contact_messages_controller.rb
 class Admin::ContactMessagesController < Admin::BaseController
 before_action :set_contact_message, only: [:show, :destroy]
 
 def index
-@contact_messages = ContactMessage.order(created_at: :desc)
+@contact_messages = ContactMessage.order(created_at: :desc).to_a
 end
 
 def show
 end
 
 def destroy
-@contact_message.destroy
+@contact_message.destroy!
 redirect_to admin_contact_messages_path, notice: "削除しました"
 end
 
