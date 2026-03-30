e83f2ab6154e5bc2110f50d923cda503badbd477
soregaaashiii <abclologun@gmail.com>
2026-03-31 03:59:09 +0900

    Move static pages controller out of concerns

--- app/controllers/concerns/static_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaticPagesController < ApplicationController
-def terms
-end
-
-def privacy
-end
-end
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+# /Users/kawamuratakuya/Desktop/吸えログデータ/dev/suelog/app/controllers/static_pages_controller.rb
+class StaticPagesController < ApplicationController
+def terms
+end
+
+def privacy
+end
+end
