50c0dcfd9c4f3c40ff222d9f1614a883d44818eb
川村拓也 <kawamuratakuya@kawamuratakuyanoMacBook-Pro.local>
2026-04-05 08:26:57 +0900

    Fix Admin::AnalyticsController constant

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -1,62 +1,68 @@
 # /Users/kawamuratakuya/dev/suelog/app/controllers/admin/articles_controller.rb
+# frozen_string_literal: true
+
 class Admin::ArticlesController < Admin::BaseController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @articles = Article.order(created_at: :desc)
-  end
+def index
+@articles = Article.order(created_at: :desc)
+end
 
-  def show
-  end
+def show
+end
 
-  def new
-    @article = Article.new(published: false)
-  end
+def new
+@article = Article.new(published: false)
+end
 
-  def create
-    @article = Article.new(article_params)
+def create
+@article = Article.new(article_params)
 
-    if @article.save
-      redirect_to admin_articles_path, notice: "記事を作成しました"
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+if @article.save
+redirect_to admin_articles_path, notice: "記事を作成しました"
+else
+render :new, status: :unprocessable_entity
+end
+end
 
-  def edit
-  end
+def edit
+end
 
-  def update
-    if @article.update(article_params)
-      redirect_to admin_articles_path, notice: "記事を更新しました"
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
+def update
+if @article.update(article_params)
+redirect_to admin_articles_path, notice: "記事を更新しました"
+else
+render :edit, status: :unprocessable_entity
+end
+end
 
-  def destroy
-    @article.destroy
-    redirect_to admin_articles_path, notice: "記事を削除しました"
-  end
+def destroy
+@article.destroy!
+redirect_to admin_articles_path, notice: "記事を削除しました"
+end
 
-  private
+private
 
-  def set_article
-    @article = Article.find_by!(slug: params[:id])
-  end
+def set_article
+@article =
+Article.find_by(slug: params[:id]) ||
+Article.find_by(id: params[:id])
 
-  def article_params
-    params.require(:article).permit(
-      :title,
-      :slug,
-      :summary,
-      :published,
-      :published_at,
-      :admin_note,
-      :seo_title,
-      :meta_description,
-      :eyecatch,
-      :body
-    )
-  end
+raise ActiveRecord::RecordNotFound, "Article not found" unless @article
+end
+
+def article_params
+params.require(:article).permit(
+:title,
+:slug,
+:summary,
+:published,
+:published_at,
+:admin_note,
+:seo_title,
+:meta_description,
+:eyecatch,
+:body
+)
+end
 end
