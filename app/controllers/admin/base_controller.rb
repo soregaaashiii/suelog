d931260cc33279756ee0385f4530e0e0a8de74ef
Takuya Kawamura <abclologun@gmail.com>
2026-04-07 22:33:13 +0900

    change admin path and basic auth credentials

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,5 @@
 class Admin::BaseController < ApplicationController
   layout "admin"
   helper Admin::NavigationHelper
-  http_basic_authenticate_with name: "admin", password: "password"
+  http_basic_authenticate_with name: "kawa_admin_92", password: "X9f!pL3#sK"
 end
