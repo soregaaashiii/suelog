d04a6296e755302b70e20b13ea33333757582bf9
Takuya Kawamura <abclologun@gmail.com>
2026-04-14 22:07:54 +0900

    fix: pagination keeps area param

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -602,7 +602,7 @@ end
   end
 
   def cleaned_listing_query
-  request.query_parameters.to_h.except("page", "commit", "area").each_with_object({}) do |(key, value), hash|
+  request.query_parameters.to_h.except("page", "commit").each_with_object({}) do |(key, value), hash|
     next if value.blank?
     next if station_route_request? && key == "station"
     next if genre_route_request? && key == "genre"
