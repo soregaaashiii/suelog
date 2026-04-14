2905876ca916800e696610de02b321cd677c0b9a
Takuya Kawamura <abclologun@gmail.com>
2026-04-14 12:33:55 +0900

    Fix namba station pagination route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -574,23 +574,10 @@ end
   end
 
   def pagination_params_for_links
-  route_params = {}
-
-  if station_route_request?
-    route_params[:station] = current_station_slug
-  end
-
-  route_params[:genre] = current_genre_slug if genre_route_request?
-  route_params[:smoking_area] = request.path_parameters[:smoking_area] if request.path_parameters[:smoking_area].present?
-
-  route_params.merge(@listing_query || {}).merge(per: @per)
+  (@listing_query || {}).merge(per: @per)
 end
 
-  route_params[:genre] = current_genre_slug if genre_route_request?
-  route_params[:smoking_area] = request.path_parameters[:smoking_area] if request.path_parameters[:smoking_area].present?
 
-  route_params.merge(@listing_query || {}).merge(per: @per)
-end
 
   def normalized_keyword_query(value)
     value.to_s
