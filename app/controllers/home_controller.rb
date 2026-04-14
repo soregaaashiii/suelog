e1650dac2c14c45dd00dd1f461b509d2f0e83473
Takuya Kawamura <abclologun@gmail.com>
2026-04-14 10:50:58 +0900

    Fix station filter clear and pagination params

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -385,6 +385,9 @@ class HomeController < ApplicationController
     slug = current_station_slug.to_s
     mapped = STATION_SLUG_MAP.dig(area_key, slug)
     return mapped if mapped.present?
+
+    fallback_mapped = STATION_SLUG_MAP.values.map { |stations| stations[slug] }.compact.first
+    return fallback_mapped if fallback_mapped.present?
     return nil if slug.blank?
 
     slug.tr("-", " ").strip
@@ -397,7 +400,7 @@ class HomeController < ApplicationController
     @current_sort = normalized_sort_param
     @open_now_only = open_now_only_param?
     @listing_query = cleaned_listing_query
-    @pagination_params = @listing_query.merge(per: @per)
+    @pagination_params = pagination_params_for_links
 
     genre_terms = effective_genre_terms
     station_q = effective_station_query
@@ -509,7 +512,10 @@ class HomeController < ApplicationController
   end
 
   def effective_station_query
-    @forced_station_keyword.presence || params[:station].to_s.strip
+    return @forced_station_label if @forced_station_label.present?
+    return @forced_station_keyword if @forced_station_keyword.present?
+
+    params[:station].to_s.strip
   end
 
   def normalized_smoking_area_param
@@ -540,6 +546,16 @@ class HomeController < ApplicationController
 
       hash[key] = value
     end
+  end
+
+  def pagination_params_for_links
+    route_params = {}
+    route_params[:area] = @current_area_key if @current_area_key.present?
+    route_params[:station] = current_station_slug if station_route_request?
+    route_params[:genre] = current_genre_slug if genre_route_request?
+    route_params[:smoking_area] = request.path_parameters[:smoking_area] if request.path_parameters[:smoking_area].present?
+
+    route_params.merge(@listing_query || {}).merge(per: @per)
   end
 
   def normalized_keyword_query(value)
