36619bb17e43f4c8ab96411d9e3a8d1e4a895928
Takuya Kawamura <abclologun@gmail.com>
2026-04-15 23:38:27 +0900

    Fix ActionText and article editor form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // /Users/kawamuratakuya/dev/suelog/app/javascript/application.js
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
 import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
