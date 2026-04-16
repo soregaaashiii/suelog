2e89ea63f7577dec681b9531e73c505ab69500fb
Takuya Kawamura <abclologun@gmail.com>
2026-04-16 21:49:14 +0900

    Fix hero_fit import in controllers application

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,11 @@
+// /Users/kawamuratakuya/dev/suelog/app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
+import "../hero_fit"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
-
-import "./hero_fit"
