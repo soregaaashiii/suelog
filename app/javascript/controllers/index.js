7bb2253893636e30b600bb8f7d42ac3e0df55dde
Takuya Kawamura <abclologun@gmail.com>
2026-04-16 21:34:39 +0900

    Fix hero_fit import path

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,7 +1,7 @@
 // /Users/kawamuratakuya/dev/suelog/app/javascript/controllers/index.js
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import "../hero_fit"
+import "./hero_fit"
 
 eagerLoadControllersFrom("controllers", application)
 
