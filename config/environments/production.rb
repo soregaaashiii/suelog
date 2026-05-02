9ec70e6746669475a94b0452a647e387fc815773
Takuya Kawamura <abclologun@gmail.com>
2026-05-03 03:42:03 +0900

    feat: ActiveStorageをR2に切り替え

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,40 +1,40 @@
-# /Users/kawamuratakuya/Desktop/吸えログデータ/dev/suelog/config/environments/production.rb
+# /Users/kawamuratakuya/dev/suelog/config/environments/production.rb
 # frozen_string_literal: true
 
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-config.enable_reloading = false
-config.eager_load = true
-config.consider_all_requests_local = false
-config.action_controller.perform_caching = true
+  config.enable_reloading = false
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
 
-config.public_file_server.headers = {
-"cache-control" => "public, max-age=#{1.year.to_i}"
-}
+  config.public_file_server.headers = {
+    "cache-control" => "public, max-age=#{1.year.to_i}"
+  }
 
-config.active_storage.service = :render
+  config.active_storage.service = :r2
 
-config.log_tags = [:request_id]
-config.logger = ActiveSupport::TaggedLogging.logger(STDOUT)
-config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
-config.silence_healthcheck_path = "/up"
-config.active_support.report_deprecations = false
+  config.log_tags = [:request_id]
+  config.logger = ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.silence_healthcheck_path = "/up"
+  config.active_support.report_deprecations = false
 
-config.cache_store = :solid_cache_store
-config.active_job.queue_adapter = :async
+  config.cache_store = :solid_cache_store
+  config.active_job.queue_adapter = :async
 
-config.action_mailer.default_url_options = {
-host: "suelog.jp",
-protocol: "https"
-}
+  config.action_mailer.default_url_options = {
+    host: "suelog.jp",
+    protocol: "https"
+  }
 
-routes.default_url_options[:host] = "suelog.jp"
-routes.default_url_options[:protocol] = "https"
+  routes.default_url_options[:host] = "suelog.jp"
+  routes.default_url_options[:protocol] = "https"
 
-config.force_ssl = true
+  config.force_ssl = true
 
-config.i18n.fallbacks = true
-config.active_record.dump_schema_after_migration = false
-config.active_record.attributes_for_inspect = [:id]
+  config.i18n.fallbacks = true
+  config.active_record.dump_schema_after_migration = false
+  config.active_record.attributes_for_inspect = [:id]
 end
