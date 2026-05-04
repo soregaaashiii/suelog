19ce0f551d5940e9b6f1ded9ada434b577b16bfa
Takuya Kawamura <abclologun@gmail.com>
2026-05-05 00:45:40 +0900

    営業終了までの残り時間表示を追加・店舗カードの緊急性強化

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,2 +1,19 @@
+# /Users/kawamuratakuya/dev/suelog/app/helpers/shops_helper.rb
+
 module ShopsHelper
+  def closing_soon_label(shop)
+    return nil unless shop.respond_to?(:open_now?) && shop.open_now?
+
+    closing_time = shop.today_closing_time rescue nil
+    return nil if closing_time.blank?
+
+    minutes_left = ((closing_time - Time.current) / 60).to_i
+    return nil if minutes_left <= 0
+
+    if minutes_left <= 120
+      "あと#{minutes_left}分で閉店"
+    else
+      nil
+    end
+  end
 end
